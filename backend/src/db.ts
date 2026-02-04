10fe96a5b71824881d8f4290f371acbeac891575
unknown <a27299@svalero.com>
2026-02-04 10:47:48 +0100

    Resolucion errores

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -3,9 +3,10 @@ import sqlite3 from "sqlite3";
 export type Db = sqlite3.Database;
 
 export function createDb() {
-sqlite3.verbose();
+    sqlite3.verbose();
 
-// Reutilizable: lo centralizamos aquí
-const filename = "data/disney.sqlite";
-return new sqlite3.Database(filename);
+    // Reutilizable: lo centralizamos aquí
+    // User requested "disney.sql" specifically
+    const filename = "data/disney.sqlite";
+    return new sqlite3.Database(filename);
 }
